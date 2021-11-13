de4714cc262b96bf4119e678ee9b7a058e6ddc79
sangtran0000006@gmail.com <sangtran0000006@gmail.com>
2021-11-13 22:17:30 +0700

    hello ne

--- app/src/main/java/com/hoangviet/diemdanh/Fragments/CanhanFragment.java
+++ app/src/main/java/com/hoangviet/diemdanh/Fragments/CanhanFragment.java
@@ -3,7 +3,6 @@ package com.hoangviet.diemdanh.Fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,19 +11,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.hoangviet.diemdanh.LoginActivity;
-import com.hoangviet.diemdanh.ProfileActivity;
 import com.hoangviet.diemdanh.R;
 
 
